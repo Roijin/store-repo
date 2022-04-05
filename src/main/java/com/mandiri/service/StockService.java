7f68b566deecee52d492659eb3a50550bce899b4
raihan.khafidzkurniawan <raihan.khafidzkurniawan@gmail.com>
2022-04-05 17:47:30 +0700

    [FEATURE] Added Stock Service

---
+++ src/main/java/com/mandiri/service/StockService.java
@@ -0,0 +1,32 @@
+package com.mandiri.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class StockService implements CRUDService{
+
+    @Override
+    public Page findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object register(Object o) {
+        return null;
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+}
