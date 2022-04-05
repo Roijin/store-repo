80cdf68c9c38c3a260c995245838e61effcb84e4
raihan.khafidzkurniawan <raihan.khafidzkurniawan@gmail.com>
2022-04-05 20:46:16 +0700

    [FEATURE] Able to validate store ID when storing new stock item

--- src/main/java/com/mandiri/service/StockService.java
+++ src/main/java/com/mandiri/service/StockService.java
@@ -1,12 +1,16 @@
 package com.mandiri.service;
 
+import com.mandiri.constants.ResponseMessage;
 import com.mandiri.entity.Stock;
+import com.mandiri.entity.Store;
 import com.mandiri.repository.StockRepository;
 import com.mandiri.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class StockService implements CRUDService<Stock, String>{
@@ -14,6 +18,8 @@ public class StockService implements CRUDService<Stock, String>{
     @Autowired
     StockRepository stockRepository;
 
+    @Autowired
+    StoreRepository storeRepository;
 
     @Override
     public Page<Stock> findAll(Pageable pageable) {
@@ -27,6 +33,10 @@ public class StockService implements CRUDService<Stock, String>{
 
     @Override
     public Stock register(Stock stock) {
+        if(!storeRepository.existsById(stock.getStoreId())){
+            String message = String.format(ResponseMessage.RESOURCE_NOT_FOUND, Store.class.getSimpleName(),stock.getStoreId());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+        }
         return stockRepository.save(stock);
     }
 
